920d41edddbbdb7f8ca586987b84cf6680a71aae
makuk <health092154@gmail.com>
2018-08-14 20:40:16 +0900

    처음

---
+++ router/main.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+module.exports = function(app) {
+    app.get('/', (req, res) => {
+        res.render('index.html');
+    });
+    app.get('/about', (req, res) => {
+        res.render('about.html');
+    });
+    app.post('/donation', (req, res) => {
+        let jsonfile = JSON.stringify(req.body);
+        fs.writeFile('../HotCoinDonation/data/form-data.json', jsonfile, 'utf-8', (err) => { if(err) throw err; });
+        console.log('이름: ' + req.body.name);
+        console.log('돈: ' + req.body.price);
+        console.log('글: ' + req.body.paragraph);
+    });
+}
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const router = require('./router/main.js')(app);
+
+let server = app.listen(3000, () => {
+    console.log('start');
+});
