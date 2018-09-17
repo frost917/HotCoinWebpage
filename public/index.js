1319c8f78a591c77c4ab22129fcd3113efcb4ca6
makuk <health092154@gmail.com>
2018-09-18 00:09:50 +0900

    픽스#4

--- public/index.js
+++ public/index.js
@@ -1,9 +1,19 @@
-fetch('/coins/post', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify( { id: document.getElementById('userid').innerHTML, coin: document.getElementById('usercoin').innerHTML })
-})
-.then(res => res.json())
-.then(res => console.log('코인 써라'));
+document.getElementById('form1').addEventListener('submit', function() {
+    let usercoin = document.getElementById('usercoin').innerHTML;
+    let price = document.getElementById('price').value;
+    if(price > usercoin) {
+        return;
+    }
+    fetch('/coins/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            id: document.getElementById('userid').innerHTML, 
+            coin: usercoin - price
+        })
+    })
+    .then(res => res.json())
+    .then(res => console.log('코인 써라'));
+});
