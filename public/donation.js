74aa699150c6baca9a220934deb625329f8943b6
makuk <health092154@gmail.com>
2018-11-27 19:01:57 +0900

    queue#4

--- public/donation.js
+++ public/donation.js
@@ -1,15 +1,20 @@
-$('#formDonate').submit(function(event) {
-    event.preventDefault();
-    const data = $('#formDonate').serializeArray();
+$(document).ready(function() {
+    document.getElementById('id').value = document.getElementById('userid').innerHTML;
+    document.getElementById('coin').value = document.getElementById('usercoin').innerHTML;
+    
+    $('#formDonate').submit(function(event) {
+        event.preventDefault();
+        const data = $('#formDonate').serializeArray();
 
-    $.ajax({
-        url: '/success',
-        dataType: 'json',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(getFormData(data)),
-        success: function(data){
-            socket.emit('donated', data);
-        }
+        $.ajax({
+            url: '/success',
+            dataType: 'json',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(getFormData(data)),
+            success: function(data){
+                socket.emit('donated', data);
+            }
+        });
     });
 });
